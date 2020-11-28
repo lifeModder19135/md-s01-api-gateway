6d08ffc6710a8af241337f770bd768bb60811258
Nate Tolbert <ntolbertu85@gmail.com>
2020-11-28 10:12:11 -0500

    11282020 -- Project-wide scope -- INITIAL COMMIT;

---
+++ src/main/java/com/mdspicesmicro/apigateway/MdS01ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdS01ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdS01ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdspicesmicro/apigateway/MdS01ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdS01ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
