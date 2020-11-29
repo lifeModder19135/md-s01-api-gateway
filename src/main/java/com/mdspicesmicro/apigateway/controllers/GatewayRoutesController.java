9a99fef67124566dbc7e40bbfe33b9b5bc9f2d71
Nate Tolbert <ntolbertu85@gmail.com>
2020-11-29 01:25:56 -0500

    11282020 -- '''.controller/ -- Added package '''.controller and class GatewayRoutesController.java; These will expose public api;

---
+++ src/main/java/com/mdspicesmicro/apigateway/controllers/GatewayRoutesController.java
@@ -0,0 +1,9 @@
+package com.mdspicesmicro.apigateway.controllers;
+
+public class GatewayRoutesController {
+
+	public GatewayRoutesController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
